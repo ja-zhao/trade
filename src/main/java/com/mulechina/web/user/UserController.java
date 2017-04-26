214cfe51dbee282982ab2f3dfcdb400f8d9557c6
zhaojunfei <773152@qq.com>
2017-04-26 21:41:44 +0800

    增加相关日志支持

--- src/main/java/com/mulechina/web/user/UserController.java
+++ src/main/java/com/mulechina/web/user/UserController.java
@@ -1,5 +1,7 @@
 package com.mulechina.web.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -11,25 +13,32 @@ import com.mulechina.domain.User;
 @Controller
 @RequestMapping({ "/user" })
 public class UserController {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private UserService userService;
-	
+
 	@Value("${application.hello:Hello Angel}")
 	private String hello;
 
 	@RequestMapping(value = "/user")
 	@ResponseBody
 	public String user(String username) {
+		logger.info("查询相关账户信息.查询用户:{}",username);
 		User user = userService.findUserByUsername(username);
 		return user.getUsername() + "-----" + user.getPassword();
 	}
 
 	@RequestMapping(value = "/checklogin")
 	public String checklogin(User user) {
+		logger.info("验证用户名:{} 密码:{}",user.getUsername(),user.getPassword());
 		int cnt = userService.checklogin(user);
-		if(cnt!=0){
+		if (cnt != 0) {
+			logger.info("登录成功");
 			return "success";
 		}
+		logger.info("登录失败");
 		return "fail";
 	}
 }
