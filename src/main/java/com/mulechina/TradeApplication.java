4cc4c079ae5d5e7e20be652ccea8cf4f8443718f
zhaojunfei <773152@qq.com>
2017-04-26 20:18:47 +0800

    .......

--- src/main/java/com/mulechina/TradeApplication.java
+++ src/main/java/com/mulechina/TradeApplication.java
@@ -2,9 +2,10 @@ package com.mulechina;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TradeApplication {
+public class TradeApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TradeApplication.class, args);
