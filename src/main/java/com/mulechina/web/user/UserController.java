648736e9f82ae17806d89396a2a72d21654781f7
zhaojunfei <773152@qq.com>
2017-04-26 20:59:06 +0800

    增加jsp页面支持

--- src/main/java/com/mulechina/web/user/UserController.java
+++ src/main/java/com/mulechina/web/user/UserController.java
@@ -1,6 +1,7 @@
 package com.mulechina.web.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,7 +13,10 @@ import com.mulechina.mapper.UserMapper;
 @RequestMapping({ "/home" })
 public class UserController {
 	@Autowired
-	UserMapper userMapper;
+	private UserMapper userMapper;
+	
+	@Value("${application.hello:Hello Angel}")
+	private String hello;
 
 	@RequestMapping(value = "/user")
 	@ResponseBody
@@ -20,11 +24,12 @@ public class UserController {
 		User user = userMapper.findUserByName("zhaojunfei");
 		return user.getUsername() + "-----" + user.getPassword();
 	}
-	
+
 	@RequestMapping(value = "/index")
 	public String index() {
-		System.out.println("12123s");
+		System.out.println("12123s"+hello);
 		User user = userMapper.findUserByName("zhaojunfei");
+		System.out.println("12123s" + hello + user);
 		return "index";
 	}
 }
